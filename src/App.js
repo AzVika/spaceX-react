86eccc50827326e95eb4aebe3fbaf9f0719ea37a
AzVika <justvix@ukr.net>
2020-10-19 12:31:44 +0300

    day 3

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React from 'react';
+import { BrowserRouter, Route } from 'react-router-dom';
+
 import Header from './componets/Header/Header';
+import Home from './componets/Home/Home';
 import Main from './componets/Main/Main';
 import Features from './componets/Features/Features';
 import Footer from './componets/Footer/Footer';
-// import Calendar from './componets/Calendar/Calendar';
-// import Details from './componets/Details/Details';
+import Calendar from './componets/Calendar/Calendar';
+import Details from './componets/Details/Details';
 
 import FetchData from './service/FetchData';
 
@@ -18,10 +21,12 @@ class App extends React.Component {
     rocket: 'Falcon 1',
     rocketFeatures: null,
     rockets: [],
+    company: null,
   };
 
   componentDidMount() {
     this.updateRocket();
+    this.updateCompany();
   }
 
   updateRocket() {
@@ -38,17 +43,37 @@ class App extends React.Component {
     this.setState({
       rocket
     }, this.updateRocket);
-  }
+  };
+
+  updateCompany = () => {
+    this.fetchData.getCompany()
+      .then(company => this.setState({ company }));
+  };
 
   render() {
-    console.log(this.state);
     return (
-      <>
+      <BrowserRouter>
         <Header rockets={this.state.rockets} changeRocket={this.changeRocket} />
-        <Main rocket={this.state.rocket} />
-        <Features />
-        <Footer />
-      </>
+        <Route exact path='/'>
+          {this.state.company && <Home company={this.state.company} />}
+        </Route>
+
+        <Route path='/rocket'>
+          <Main rocket={this.state.rocket} />
+          {this.state.rocketFeatures &&
+            <Features {...this.state.rocketFeatures} />}
+        </Route>
+
+        <Route path='/calendar'>
+          <Calendar />
+        </Route>
+        
+        <Route path='/details'>
+          <Details />
+        </Route>
+
+        {this.state.company && <Footer {...this.state.company} />}
+      </BrowserRouter>
     );
   }
 }
